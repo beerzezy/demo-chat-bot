170e7748a47341debf37e33f2b4a5d95ad9adfb2
BeerZezy <phanudetkawilai2016@gmail.com>
2020-04-11 04:21:11 +0700

    bodyParser

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
+
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.get('/', (req, res) => {
     //res.json({ message: 'BEER!' })
     res.send('Welcome to Chat Bot API...')
 })
 
-app.post('/webhook', (req, res) => {
-    console.log(req)
+app.post('/webhook', urlencodedParser, (req, res) => {
+    console.log(req.body)
     res.status(200).send({ status: '200 OK' })
 })
 
