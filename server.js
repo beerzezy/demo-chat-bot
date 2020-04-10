1f07076a2484927006658eb0ddb18dc2cefff0e0
BeerZezy <phanudetkawilai2016@gmail.com>
2020-04-11 04:06:37 +0700

    body

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
 })
 
 app.post('/webhook', (req, res) => {
-    console.log(req.headers)
+    console.log(req.body)
     res.status(200).send({ status: '200 OK' })
 })
 
