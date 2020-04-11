5ac9256a5a061bc46ce06d5171e07cd6a2b323dc
BeerZezy <phanudetkawilai2016@gmail.com>
2020-04-11 12:07:10 +0700

    port

--- server.js
+++ server.js
@@ -53,6 +53,6 @@ app.get('/', (req, res) => {
     res.send('Welcome to Chat Bot API...')
 })
 app.listen(port, () => {
-    console.log(`Server is running at port: ${port}`);
+    console.log(`Server is running at port :: ${port}`);
 })
 
